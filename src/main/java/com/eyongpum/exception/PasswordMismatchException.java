000f70ca336659168a60a8478cc19e81b8de6fc4
chrisdohyung <chris.dohyung@gmail.com>
2021-05-25 15:32:03 +0900

    password mismatch exception

---
+++ src/main/java/com/eyongpum/exception/PasswordMismatchException.java
@@ -0,0 +1,15 @@
+package com.eyongpum.exception;
+
+public class PasswordMismatchException extends Exception {
+	public PasswordMismatchException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordMismatchException(String msg) {
+		super(msg);
+		
+	}
+	
+	
+
+}
