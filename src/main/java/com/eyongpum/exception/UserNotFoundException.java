73200183a8f0def90c70d5fd5a65bdd2c2050eb6
chrisdohyung <chris.dohyung@gmail.com>
2021-05-25 15:32:51 +0900

    user not found exception

---
+++ src/main/java/com/eyongpum/exception/UserNotFoundException.java
@@ -0,0 +1,14 @@
+package com.eyongpum.exception;
+
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public UserNotFoundException(String msg) {
+		super(msg);
+	}
+	
+
+}
