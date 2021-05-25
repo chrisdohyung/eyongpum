578818ee506f5611a368af33ddb5af23c62d3279
chrisdohyung <chris.dohyung@gmail.com>
2021-05-25 20:06:51 +0900

    BoardException

---
+++ src/main/java/com/eyongpum/board/BoardException.java
@@ -0,0 +1,18 @@
+package com.eyongpum.board;
+
+//회원에 관련된 사용자 정의 예외 클래
+public class BoardException extends Exception {
+	public BoardException() {
+		this("게시판 관련 에러 발생 ");
+	}
+
+	public BoardException(String errorMsg) {
+		super(errorMsg);
+	}
+
+	public BoardException(String errorMsg, Throwable cause) {
+		super(errorMsg, cause);
+	}
+	
+
+}
