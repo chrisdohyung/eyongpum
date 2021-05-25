c952258123b933efb30ff4d58f68d7a70f840174
chrisdohyung <chris.dohyung@gmail.com>
2021-05-25 22:31:55 +0900

    board service

--- src/main/java/com/eyongpum/board/BoardService.java
+++ src/main/java/com/eyongpum/board/BoardService.java
@@ -13,5 +13,38 @@ public class BoardService {
 		}
 		return _instance;
 	}
+	
+	public int remove(int boardNo) throws Exception, BoardException {
+		Board tempBoard = boardDao.findBoard(boardNo);
+		boolean rExist = boardDao.countReplay(tempBoard);
+		System.out.println("reply exist : "+ rExist);
+		if (boardDao.countReplay(tempBoard)) {
+			throw new BoardException("You cannot remove the board with reply.");
+		} else {
+			return boardDao.remove(tempBoard.getBoardNo());
+		}
+	}
+	
+	public int create(Board board) throws Exception {
+		return boardDao.create(board);
+	}
+	
+	public int createReply(Board board) throws Exception {
+		return boardDao.createReply(board);
+	}
+	
+	public Board findBoard(int boardNo) throws Exception {
+		Board board = boardDao.findBoard(boardNo);
+		return board;
+	}
+	
+	public void updateHitCount(int boardNo) throws Exception {
+		boardDao.increaseReadCount(boardNo);
+	}
+	
+	public BoardListPageMakerDto findBoardList(int currentPage) throws Exception {
+		int totalRecordCount = boardDao.getBoardCount();
+		PageMaker pageMaker = new PageMaker(totalRecordCount,currentPage);
+	}
 
 }
