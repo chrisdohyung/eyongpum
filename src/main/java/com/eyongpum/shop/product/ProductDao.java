be985b0ffa222ee1fbabd0fb9d4b032bf7c92f1b
chrisdohyung <chris.dohyung@gmail.com>
2021-05-28 14:38:44 +0900

    basicDataSource ..

--- src/main/java/com/eyongpum/shop/product/ProductDao.java
+++ src/main/java/com/eyongpum/shop/product/ProductDao.java
@@ -5,16 +5,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
-import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class ProductDao {
 	private DataSource dataSource;
-
+	
+	/*
 	public ProductDao() throws Exception {
 		InitialContext ic = new InitialContext();
 		dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/OracleDB");
+	}
+	*/
+	
+	public ProductDao() throws Exception {
+		Properties properties = new Properties();
+		properties.load(this.getClass().getResourceAsStream("/com/youngpum/shop/db.properties"));
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setDriverClassName(properties.getProperty("driverClass"));
+		basicDataSource.setUrl(properties.getProperty("url"));
+		basicDataSource.setUsername(properties.getProperty("user"));
+		basicDataSource.setPassword(properties.getProperty("password"));
+		dataSource = basicDataSource;
 	}
 
 	public List<Product> getProductList() throws Exception {
